e8a75546f2ea4ea8ebbfab78781a6297b076fef1
Felix Cadieux <cadieuxfelix@outlook.com>
2025-04-27 15:20:07 -0400

    april 27th

--- Project/card-info-viewer/src/app/card-input-form/card-input-form.component.ts
+++ Project/card-info-viewer/src/app/card-input-form/card-input-form.component.ts
@@ -11,6 +11,7 @@ import { CardData } from '../card-data.model';
 export class CardInputFormComponent {
   name = ''
   cardNumber = ''
+  displayNumber = ''
   expirationMonth = ''
   expirationYear = ''
   cvc = ''
@@ -47,18 +48,47 @@ export class CardInputFormComponent {
 
   validateCardNumber(){
 
+    // remove spaces from input values
+    this.cardNumber = this.displayNumber.replace(/\s+/g, '');
+    
+    // Format for display: add space every 4 chars
+    const groups = this.cardNumber.match(/.{1,4}/g);
+    this.displayNumber = groups ? groups.join(' ') : '';
+
+    if(/^(?:4[0-9]{12}(?:[0-9]{3})?|5[1-5][0-9]{14}|3[47][0-9]{13}|3(?:0[0-5]|[68][0-9])[0-9]{11}|6(?:011|5[0-9]{2})[0-9]{12}|(?:2131|1800|35\d{3})\d{11})$/i.test(this.cardNumber)  ){
+      this.validCardNumber = true;
+      this.cardNumberOutput.emit(this.cardNumber)
+    }else{
+      this.validCardNumber = false;
+    }
   }
 
   validateExpirationMonth(){
-
+    if(String(this.expirationMonth)?.length === 2){
+      this.validExpirationMonth = true
+      this.expirationMonthOutput.emit(Number(this.expirationMonth))
+    }
+    else{
+      this.validExpirationMonth = false
+    }
   }
 
   validateExpirationYear(){
-
+    if(String(this.expirationYear)?.length === 2){
+      this.validExpirationYear = true;
+      this.expirationYearOutput.emit(Number(this.expirationYear))
+    }else{
+      this.validExpirationYear = false;
+    }
   }
 
   validateCVC(){
-
+    if(String(this.cvc)?.length === 3){
+      this.validCVC = true;
+      this.cvcOutput.emit(Number(this.cvc))
+    }else{
+      this.validCVC = false
+    }
   }
 
 }
